0382460f3eb88f190f8005f52f8f13bdb6413ca3
chloekaii <116681534+chloekaii@users.noreply.github.com>
2024-03-27 18:07:23 -0400

    improved the structure of the database (.php file will probably break tho)

--- database.sql
+++ database.sql
@@ -1,47 +1,51 @@
-CREATE DATABASE IF NOT EXISTS userDatabase;
-USE userDatabase;
+CREATE DATABASE IF NOT EXISTS TimeTracker; 
+USE TimeTracker;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(255),
-    employee_id INT
+CREATE TABLE User (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100) UNIQUE,
+    Password VARCHAR(255),
+    EmployeeID INT
 );
 
-CREATE TABLE userRoles ( 
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    employee_id INT,
-    user_role ENUM('employee', 'manager', 'admin') NOT NULL
+CREATE TABLE Role (
+    RoleID INT AUTO_INCREMENT PRIMARY KEY,
+    RoleName VARCHAR(50)
 )
 
-CREATE TABLE projects ( 
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    project_id INT,
-    project_title VARCHAR(100)
+CREATE TABLE UserRole ( 
+    UserRoleID INT AUTO_INCREMENT PRIMARY KEY,
+    UserID INT,
+    RoleID INT,
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (RoleID) REFERENCES Role(RoleID)
 )
 
-CREATE TABLE TimeEntries (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    employee_id INT,
-    project_id INT,
-    clock_in_time DATETIME,
-    clock_out_time DATETIME,
-    description VARCHAR(255), --employees can add a description of their work done
-    FOREIGN KEY (employee_id) REFERENCES users(employee_id),
-    FOREIGN KEY (project_id) REFERENCES projects(project_id),
-    CONSTRAINT FK_employeeProject -- ensures that employees can only attribute time entries to projects they have been assigned
-        FOREIGN KEY (employee_id, project_id) 
-        REFERENCES employeeProjects(employee_id, project_id)
+CREATE TABLE Project ( 
+    ProjectID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(200),
+    Description VARCHAR(1000)
+)
+
+CREATE TABLE TimeEntry (
+    TimeEntryID INT AUTO_INCREMENT PRIMARY KEY,
+    UserID INT,
+    ProjectID INT,
+    ClockInTime DATETIME,
+    ClockOutTime DATETIME,
+    Description VARCHAR(1000), 
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID),
 );
 
 
-CREATE TABLE employeeProjects (
-    employee_id INT,
-    project_id INT,
-    PRIMARY KEY (employee_id, project_id),
-    FOREIGN KEY (employee_id) REFERENCES users(employee_id),
-    FOREIGN KEY (project_id) REFERENCES Projects(project_id)
+CREATE TABLE ProjectAssignment (
+    ProjectAssignmentID INT AUTO_INCREMENT PRIMARY KEY,
+    UserID INT,
+    ProjectID INT,
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID)
 );
 
